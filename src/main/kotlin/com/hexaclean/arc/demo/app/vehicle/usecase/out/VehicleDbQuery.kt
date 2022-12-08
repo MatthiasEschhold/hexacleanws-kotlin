37654fa2425472a89f7969c5bc0f33421c6b4bab
MatthiasEschhold <84323232+MatthiasEschhold@users.noreply.github.com>
2022-12-08 06:59:21 +0100

    lab2

--- src/main/kotlin/com/hexaclean/arc/demo/app/vehicle/usecase/out/VehicleDbQuery.kt
+++
@@ -1,8 +0,0 @@
-package com.hexaclean.arc.demo.app.vehicle.usecase.out
-
-import com.hexaclean.arc.demo.app.vehicle.domain.model.Vehicle
-import com.hexaclean.arc.demo.app.vehicle.domain.model.Vin
-
-interface VehicleDbQuery {
-    fun findVehicleByVin(vin: Vin): Vehicle
-}
